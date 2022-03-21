31fe8423154ac903a4327e7c45ab8f6c90c478ec
Noman Sanjari <nomansanjari2001@gmail.com>
2022-03-21 07:41:40 -0600

    added Developer

---
+++ src/main/java/BugIO/Developer/Developer.java
@@ -0,0 +1,36 @@
+package BugIO.Developer;
+
+import java.util.UUID; // for generating unique IDs
+import java.util.ArrayList;
+
+public class Developer {
+    private UUID ID;
+    private String firstName;
+    private String lastName;
+    private ArrayList<UUID> Projects;
+
+    public void Developer(String fName, String lName)
+    {
+        this.ID = UUID.randomUUID();
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+
+    // setter methods
+
+    // getter methods
+    public UUID getID()
+    {
+        return this.ID;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+}
